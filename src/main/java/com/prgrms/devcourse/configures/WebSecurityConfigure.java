65558f52e51876cfb9f6db3bf05a870db9b9f35c
bona <ra_ari@naver.com>
2023-08-08 19:22:27 -0700

    ChannelProcessingFilter

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -1,32 +1,52 @@
 package com.prgrms.devcourse.configures;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
   @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+            .withUser("user").password("{noop}user123").roles("USER")
+            .and()
+            .withUser("admin").password("{noop}admin123").roles("ADMIN");
+  }
+
+  @Override // 전역설정
   public void configure(WebSecurity web) {
     web.ignoring().antMatchers("/assets/**");
   }
 
-  @Override
+  @Override // 세부적인 보안기능(필터 설정)
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .authorizeRequests()
-        .antMatchers("/me").hasAnyRole("USER", "ADMIN")
-        .anyRequest().permitAll()
-        .and()
-      .formLogin()
-        .defaultSuccessUrl("/")
-        .permitAll()
-        .and()
-    ;
+            .authorizeRequests()
+            .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+            .anyRequest().permitAll()
+            .and()
+            .formLogin()
+            .defaultSuccessUrl("/")
+            .permitAll()
+            .and()
+            .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/")
+            .invalidateHttpSession(true)
+            .clearAuthentication(true)
+            .and()
+            .rememberMe()
+            .rememberMeParameter("remember-me")
+            .tokenValiditySeconds(300)
+            .and()
+            .requiresChannel()
+            .anyRequest().requiresSecure();//모든 채널은 https 서비스해야한다 를 설정하는것
   }
 
 }
